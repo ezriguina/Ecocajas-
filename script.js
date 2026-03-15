763423137e7b6403a281df1f771b304b2530683d
Elbachir <elbachirzriguinat@gmail.com>
2026-03-15 20:38:26 +0100

    okok

--- script.js
+++ script.js
@@ -12,3 +12,30 @@ e.preventDefault();
 alert("Gracias por contactar con ECOCAJAS 🌱");
 
 });
+document.querySelector(".contact-form").addEventListener("submit", function(e){
+
+e.preventDefault()
+
+alert("Gracias por contactar con ECOCAJAS 🌱")
+
+})
+
+
+const sections = document.querySelectorAll(".section")
+
+window.addEventListener("scroll",()=>{
+
+sections.forEach(section=>{
+
+const top = section.getBoundingClientRect().top
+
+if(top < window.innerHeight - 100){
+
+section.style.opacity = 1
+section.style.transform = "translateY(0)"
+
+}
+
+})
+
+})
